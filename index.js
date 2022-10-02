e2d33784e6f85a1e2419515fdce7bf55d1a84f9c
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-10-02 15:54:29 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const Sheet = () => {
+	const data = {};
+
+	const init = ({ apiKey, sheetId, sheetName }) => {
+		data.apiKey = apiKey;
+		data.sheetId = sheetId;
+		data.url = `https://api.sheetson.com/v2/sheets/${sheetName}`;
+	};
+
+	const getReq = async (url) => {
+		const req = await fetch(url, {
+			headers: {
+				Authorization: `Bearer ${data.apiKey}`,
+				"X-Spreadsheet-Id": data.sheetId,
+			},
+		});
+
+		const res = await req.json();
+		return res;
+	};
+
+	const postReq = async (body, url) => {
+		const req = await fetch(url, {
+			method: "POST",
+			headers: {
+				Authorization: `Bearer ${data.apiKey}`,
+				"X-Spreadsheet-Id": data.sheetId,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(body),
+		});
+
+		const res = await req.json();
+		return res;
+	};
+
+	const patchReq = async (body, url, row) => {
+		const req = await fetch(`${url}/${row}`, {
+			method: "PATCH",
+			headers: {
+				Authorization: `Bearer ${data.apiKey}`,
+				"X-Spreadsheet-Id": data.sheetId,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(body),
+		});
+
+		const res = await req.json();
+		return res;
+	};
+
+	const addRow = async (body, callback) => {
+		const result = await postReq(body, data.url);
+		callback && callback(result);
+	};
+
+	const getAllRows = async (callback) => {
+		const { results } = await getReq(data.url);
+		callback(results);
+	};
+
+	const getRow = async (column, value, callback) => {
+		let rows;
+		await getAllRows((data) => (rows = data));
+
+		const match = rows.find((elem) => {
+			return (
+				elem &&
+				elem.hasOwnProperty(column) &&
+				Object.values(elem).includes(value)
+			);
+		});
+
+		callback && callback(match ?? null);
+	};
+
+	const updateRow = async (body, column, value, callback) => {
+		let rowIndex;
+		await getRow(column, value, (data) => (rowIndex = data.rowIndex));
+		const result = await patchReq(body, data.url, rowIndex);
+		callback && callback(result);
+	};
+
+	return {
+		init,
+		addRow,
+		getAllRows,
+		getRow,
+		updateRow,
+	};
+};
+
+module.exports = Sheet;
